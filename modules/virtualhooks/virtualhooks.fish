a90989d64274363bd846a36b3fce58b575025340
Justin Mayer <entroP@gmail.com>
2015-02-20 13:15:00 -0800

    Minor tweaks to virtualhooks plugin

--- modules/virtualhooks/virtualhooks.fish
+++ modules/virtualhooks/virtualhooks.fish
@@ -1,7 +1,7 @@
 # Virtualhooks: https://github.com/justinmayer/tackle/tree/master/modules/virtualhooks
 function virtualhooks --on-event virtualenv_did_activate
-    if test -f $VIRTUAL_ENV/virtualhooks.fish
+    if test -f "$VIRTUAL_ENV/virtualhooks.fish"
         echo "Sourcing $VIRTUAL_ENV/virtualhooks.fish"
-        . $VIRTUAL_ENV/virtualhooks.fish
+        source "$VIRTUAL_ENV/virtualhooks.fish"
     end
 end
